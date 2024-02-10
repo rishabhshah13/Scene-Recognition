c691754730cc96bdf5d37478289ec258e0ceb816
rishabhshah13 <rishabhshah13@gmail.com>
2024-02-10 13:52:06 -0500

    main and train added

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ from datetime import datetime
 
 
 
-def train(model, train_dataloader, val_dataloader, num_epochs, learning_rate, save_checkpoints):
+def train(model, train_dataloader, val_dataloader, num_epochs, learning_rate, save_checkpoints,run_dir):
     
 
     device = 'cuda' if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else 'cpu')
@@ -55,7 +55,13 @@ def train(model, train_dataloader, val_dataloader, num_epochs, learning_rate, sa
             total_imgs += len(target)
         train_loss.append(sum(batch_loss) / len(train_dataloader))
         train_metrics.append(sum(batch_metric) / total_imgs)
+        print(f"Training Metric --- Train Accuracy: {sum(batch_metric)/total_imgs} ---- Train Loss: {sum(np.array(batch_loss)/len(train_dataloader))}")
 
+        del data 
+        del target
+        del pred
+        del loss
+        
         # Validation phase
         model.eval()
         batch_metric = []
@@ -72,104 +78,15 @@ def train(model, train_dataloader, val_dataloader, num_epochs, learning_rate, sa
                 total_imgs += len(target)
         val_loss.append(sum(batch_loss) / len(val_dataloader))
         val_metrics.append(sum(batch_metric) / total_imgs)
+        print(f"Validation Metric --- Val Accuracy: {sum(batch_metric)/total_imgs} ---- Val Loss: {sum(np.array(batch_loss)/len(val_dataloader))}")
 
         # Save checkpoint if required
         if epoch in save_checkpoints:
-            torch.save(model.state_dict(), f'{epoch}.chkpt')
+            print(f'Saving {run_dir}/{epoch}.chkpt')
+            torch.save(model.state_dict(), f'{run_dir}/{epoch}.chkpt')
+       
+        if device == 'mps':
+            torch.mps.empty_cache()
 
     return train_loss, val_loss, train_metrics, val_metrics
 
-
-
-
-# define model 
-model = models[model_base]()
-model.to(device)
-
-# define optimizer and criterion
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-criterion = nn.CrossEntropyLoss()
-
-# training loop
-train_loss = []
-val_loss = []
-test_loss = []
-train_metrics = []
-val_metrics = []
-for epoch in range(num_epochs):
-    print(f"epoch: {epoch}")
-    log(f'epoch {epoch}')
-    #training
-    model.train()
-    batch_loss = []
-    batch_metric = []
-    total_imgs = 0
-    for i, (_data, _target) in tqdm(enumerate(train_dataloader)): 
-        data = _data.to(device)
-        target = _target.to(device)
-        optimizer.zero_grad()
-        pred = model(data)
-        loss = criterion(pred, target)
-        loss.backward()
-        optimizer.step()
-        batch_loss.append(loss.item())
-        batch_metric.append(sum(torch.argmax(pred, dim=1)==target).item())
-        total_imgs += len(target)
-    train_loss.append(sum(np.array(batch_loss)/len(train_dataloader)))
-    log(f'\ttrain loss: {train_loss[-1]}')
-    train_metrics.append(sum(batch_metric)/total_imgs) #TODO: add metrics
-    print(f"Training Metric --- Train Accuracy: {sum(batch_metric)/total_imgs} ---- Train Loss: {sum(np.array(batch_loss)/len(train_dataloader))}")
-    del data 
-    del target
-    del pred
-    del loss
-
-    # validation
-    total_imgs = 0
-    batch_metric = []
-    with torch.no_grad():
-        model.eval()
-        batch_loss = []
-        for i, (_data, _target) in tqdm(enumerate(val_dataloader)): 
-            data = _data.to(device)
-            target = _target.to(device)
-            pred = model(data)
-            loss = criterion(pred, target)
-            batch_loss.append(loss.item())
-            batch_metric.append(sum(torch.argmax(pred, dim=1)==target).item())
-            total_imgs += len(target)
-        val_loss.append(sum(np.array(batch_loss)/len(val_dataloader)))
-        log(f'\tval loss: {val_loss[-1]}')
-        val_metrics.append(sum(batch_metric)/total_imgs) #TODO: add metrics
-        print(f"Validation Metric --- Val Accuracy: {sum(batch_metric)/total_imgs} ---- Val Loss: {sum(np.array(batch_loss)/len(val_dataloader))}")
-
-
-    if epoch in save_chks: 
-        torch.save(model.state_dict(), os.path.join(run_dir, f'{epoch}.chkpt'))
-
-    plt.plot(train_loss, label='train')
-    plt.plot(val_loss, label='val')
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.legend()
-    plt.savefig(os.path.join(run_dir, 'loss'))
-    plt.close()
-    plt.plot(train_metrics, label='train accuracy')
-    plt.plot(val_metrics, label='val accuracy')
-    plt.xlabel('epoch')
-    plt.ylabel('accuracy')
-    plt.legend()
-    plt.savefig(os.path.join(run_dir, 'accu'))
-    plt.close()
-    del data 
-    del target
-    del pred
-    del loss
-
-    if device == 'mps':
-        torch.mps.empty_cache()
-
-
-# testing
-with torch.no_grad():
-    model.eval()
